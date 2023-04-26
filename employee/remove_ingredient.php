ba85fdb63986bf4e394d1c8c443a492b8f6d805b
Syed Asad <syed12@tamu.edu>
2023-04-26 16:05:36 -0500

    can remove ingredients from items

---
+++ employee/remove_ingredient.php
@@ -0,0 +1,38 @@
+<?php
+// Get form data
+$itemname = $_POST["itemname"];
+$ingredientname = $_POST["ingredientname"];
+
+require_once("../connect_db.php");
+$conn = connect_mysql();
+
+try{
+    // Remove from association
+    $associate =    "DELETE FROM ItemIngredient
+                    WHERE item_id = (SELECT item_id FROM Item WHERE item_name = '$itemname')
+                        AND ingredient_id = (SELECT ingredient_id FROM Ingredient WHERE ingredient_name = '$ingredientname')";
+    mysqli_query($conn, $associate);
+
+    mysqli_commit($conn);
+
+    echo "Ingredient removed successfully";
+}
+catch (\Throwable $e) {
+    // An exception thrown, rollback...
+    $conn->rollback();
+
+    // catch error on distinct username/email: TODO
+    echo $e->getCode();
+    if (mysqli_errno($e) == 1062) {
+        print 'no way!';
+    }
+
+    throw $e; // handle exception
+}
+
+// Close connection
+mysqli_close($conn);
+
+header("Location: admin_menu.php");
+
+?>
