d387a5c07ab62fcfe9b9102ae8eaf82a7d346127
ekdev <ekdev@tamu.edu>
2023-04-26 20:40:05 -0500

    changes to customer review and process review pages for upsert

--- customer/customer_review.php
+++ customer/customer_review.php
@@ -11,5 +11,28 @@
         <form action="process_review.php" method="get">
                 <input type="submit" value="Create/Edit Review">
         </form>
+        <?php 
+            // How to get parameters from the query string
+            // https://www.w3docs.com/snippets/php/get-url-query-string-parameters.html#:~:text=To%20get%20the%20query%20string,of%20the%20query%20string%20parameters.&text=Note%20that%20if%20the%20parameter,if%20a%20parameter%20is%20set.
+            $customerId = json_decode($_COOKIE["currentUser"], true);
+
+            require_once("../connect_db.php"); // importing function
+            $conn = connect_mysql();
+
+            $reviewSql = "SELECT * FROM `review` WHERE customer_id = '$customerId'";
+            $reviewResult = mysqli_query($conn, $reviewSql);
+
+            // Display all the review items. 
+            while ($reviewRow = mysqli_fetch_assoc($reviewResult)) {
+                    echo "<p>Review Content: " . $reviewRow['review'] . "</p>";
+                    echo "<p>Review Rating: " . $reviewRow['rating'] . "</p>";
+                    echo "<p>Time Reviewed: " . $reviewRow['datetime'] . "</p>";
+            }
+            // Close connection
+            mysqli_close($conn);
+        ?>
 </body>
 </html>
+
+
+
--- customer/process_review.php
+++ customer/process_review.php
@@ -5,10 +5,23 @@
 </head>
 <body>
     <h1>Add Review</h1>
+    <a href="home.php">Home</a>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-        <label for="item_id">Item ID:</label>
-        <input type="text" id="item_id" name="item_id" required><br>
+        <label for="item_id">Item Name:</label>
+        <select id="item_id" name="item_id" required>
+            <?php
+            $customer_id = json_decode($_COOKIE["currentUser"], true);
+            require_once("../connect_db.php");
+            $conn = connect_mysql();
+            $sql = "SELECT item_id, item_name FROM user_item_view uiv WHERE uiv.customer_id = '$customer_id'";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<option value='".$row['item_id']."'>".$row['item_name']."</option>";
+            }
+            mysqli_close($conn);
+            ?>
+        </select><br>
 
         <label for="review">Review:</label>
         <textarea id="review" name="review" rows="5" cols="40" required></textarea><br>
@@ -38,21 +51,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $item_id = $_POST["item_id"];
     $review = $_POST["review"];
     $rating = $_POST["rating"];
+    
 
     if (empty($item_id) || empty($review) || empty($rating)) {
         echo "Please fill out all required fields.";
     } else {
         // Insert review into database
-        $insertSql = "INSERT INTO Review (customer_id, item_id, review, rating)
-                      VALUES ('$customer_id', '$item_id', '$review', '$rating')";
-        // $stmt = mysqli_prepare($conn, "INSERT INTO Review (`customer_id`, `item_id`, `review`, `rating`) VALUES (?, ?, ?, ?)");
-        // mysqli_stmt_bind_param($stmt, "iisi", $customer_id, $item_id, $review, $rating);
-        // if (mysqli_stmt_execute($stmt)) {
-        //     echo "Review added successfully.";
-        // } else {
-        //     echo "Error adding review: " . mysqli_stmt_error($stmt);
-        // }
-        // mysqli_stmt_close($stmt);
+        $upsertSql = "INSERT INTO Review (customer_id, item_id, review, rating)
+                        VALUES ('$customer_id', '$item_id', '$review', '$rating')
+                      ON DUPLICATE KEY UPDATE
+                        review = '$review',
+                        rating = '$rating'
+                        ";
         mysqli_query($conn, $insertSql);
     }
     mysqli_close($conn);
